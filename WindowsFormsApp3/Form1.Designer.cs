5b84b7a7ce654cd0775c6d7a24eadabff1276d14
STI-ADMIN <STI-ADMIN@LAB-A-PC33>
2024-04-26 09:53:25 +0800

    Add project files.

---
+++ WindowsFormsApp3/Form1.Designer.cs
@@ -0,0 +1,282 @@
+﻿namespace WindowsFormsApp3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.Radio1 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.label6 = new System.Windows.Forms.Label();
+            this.Combo1 = new System.Windows.Forms.ComboBox();
+            this.Combo2 = new System.Windows.Forms.ComboBox();
+            this.Combo3 = new System.Windows.Forms.ComboBox();
+            this.Box1 = new System.Windows.Forms.TextBox();
+            this.Box2 = new System.Windows.Forms.TextBox();
+            this.Box3 = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.Combo4 = new System.Windows.Forms.ComboBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button2 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(57, 102);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(129, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Student Registration Form";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(62, 129);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(62, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Last Name*";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(62, 168);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(61, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "First Name*";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(62, 214);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(73, 13);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Middle Name*";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(60, 261);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(46, 13);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "Gender*";
+            // 
+            // Radio1
+            // 
+            this.Radio1.AutoSize = true;
+            this.Radio1.Location = new System.Drawing.Point(122, 257);
+            this.Radio1.Name = "Radio1";
+            this.Radio1.Size = new System.Drawing.Size(52, 17);
+            this.Radio1.TabIndex = 5;
+            this.Radio1.TabStop = true;
+            this.Radio1.Text = "Male*";
+            this.Radio1.UseVisualStyleBackColor = true;
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(180, 257);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(63, 17);
+            this.radioButton2.TabIndex = 6;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.Text = "Female*";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(62, 286);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(70, 13);
+            this.label6.TabIndex = 7;
+            this.label6.Text = "Date of Birth*";
+            // 
+            // Combo1
+            // 
+            this.Combo1.FormattingEnabled = true;
+            this.Combo1.Location = new System.Drawing.Point(54, 307);
+            this.Combo1.Name = "Combo1";
+            this.Combo1.Size = new System.Drawing.Size(121, 21);
+            this.Combo1.TabIndex = 8;
+            this.Combo1.Text = "-day-";
+            // 
+            // Combo2
+            // 
+            this.Combo2.FormattingEnabled = true;
+            this.Combo2.Location = new System.Drawing.Point(181, 307);
+            this.Combo2.Name = "Combo2";
+            this.Combo2.Size = new System.Drawing.Size(121, 21);
+            this.Combo2.TabIndex = 9;
+            this.Combo2.Text = "-month-";
+            // 
+            // Combo3
+            // 
+            this.Combo3.FormattingEnabled = true;
+            this.Combo3.Location = new System.Drawing.Point(308, 307);
+            this.Combo3.Name = "Combo3";
+            this.Combo3.Size = new System.Drawing.Size(121, 21);
+            this.Combo3.TabIndex = 10;
+            this.Combo3.Text = "-year-";
+            // 
+            // Box1
+            // 
+            this.Box1.Location = new System.Drawing.Point(60, 145);
+            this.Box1.Name = "Box1";
+            this.Box1.Size = new System.Drawing.Size(100, 20);
+            this.Box1.TabIndex = 11;
+            // 
+            // Box2
+            // 
+            this.Box2.Location = new System.Drawing.Point(60, 184);
+            this.Box2.Name = "Box2";
+            this.Box2.Size = new System.Drawing.Size(100, 20);
+            this.Box2.TabIndex = 12;
+            // 
+            // Box3
+            // 
+            this.Box3.Location = new System.Drawing.Point(60, 231);
+            this.Box3.Name = "Box3";
+            this.Box3.Size = new System.Drawing.Size(100, 20);
+            this.Box3.TabIndex = 13;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(51, 349);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(90, 13);
+            this.label7.TabIndex = 14;
+            this.label7.Text = "Program to apply*";
+            // 
+            // Combo4
+            // 
+            this.Combo4.FormattingEnabled = true;
+            this.Combo4.Location = new System.Drawing.Point(54, 365);
+            this.Combo4.Name = "Combo4";
+            this.Combo4.Size = new System.Drawing.Size(375, 21);
+            this.Combo4.TabIndex = 15;
+            this.Combo4.Text = "-Select Program-";
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.button1.Location = new System.Drawing.Point(54, 401);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(168, 23);
+            this.button1.TabIndex = 16;
+            this.button1.Text = "Registration Student";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.InitialImage = null;
+            this.pictureBox1.Location = new System.Drawing.Point(495, -2);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(676, 583);
+            this.pictureBox1.TabIndex = 17;
+            this.pictureBox1.TabStop = false;
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.button2.Location = new System.Drawing.Point(752, 587);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(198, 55);
+            this.button2.TabIndex = 18;
+            this.button2.Text = "Browse";
+            this.button2.UseVisualStyleBackColor = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1183, 738);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.Combo4);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.Box3);
+            this.Controls.Add(this.Box2);
+            this.Controls.Add(this.Box1);
+            this.Controls.Add(this.Combo3);
+            this.Controls.Add(this.Combo2);
+            this.Controls.Add(this.Combo1);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.radioButton2);
+            this.Controls.Add(this.Radio1);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.RadioButton Radio1;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.ComboBox Combo1;
+        private System.Windows.Forms.ComboBox Combo2;
+        private System.Windows.Forms.ComboBox Combo3;
+        private System.Windows.Forms.TextBox Box1;
+        private System.Windows.Forms.TextBox Box2;
+        private System.Windows.Forms.TextBox Box3;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.ComboBox Combo4;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ WindowsFormsApp3/Form1.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp3
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            String[] month = { "January", "February", "March", "April", "May", "June", "July", "August", "November", "December" };
+            Combo2.Items.AddRange(month);
+            foreach (String months in month)
+            {
+                Console.WriteLine(months);
+            }
+
+            ArrayList day = new ArrayList();
+            for (int i = 0; i < 31; i++)
+            {
+                day.Add(i + 1);
+                Combo1.Items.Add(day[i]);
+
+            }
+
+            ArrayList year = new ArrayList();
+            for (int i = 1900; i <= 2024; i++)
+            {
+                year.Add(i);
+                Combo3.Items.Add(year[i - 1900]);
+            }
+
+            ArrayList program = new ArrayList();
+            program.Add("Bachelor of Science in Computer Science");
+            program.Add("Bachelor of Science in Information Technology");
+            program.Add("Bachelor of Science in Information Systems");
+            program.Add("Bachelor of Science in Computer Engineering");
+            Combo4.Items.AddRange(program.ToArray());
+            foreach (String programName in program)
+            {
+                Console.Write(programName);
+            }
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            String month = Combo2.GetItemText(Combo2.SelectedItem);
+            String day = Combo1.GetItemText(Combo1.SelectedItem);
+            String year = Combo3.GetItemText(Combo3.SelectedItem);
+            String gender = Radio1.Checked == true ? "Male" : "Female";
+
+            String fn = Box2.Text;
+            String mn = Box3.Text;
+            String ln = Box1.Text;
+            String program = Combo4.Text;
+
+            RegisterStudent(ln, fn, mn, gender, day, month, year, program);
+            RegisterStudent(ln, fn, mn, program);
+            RegisterStudent(ln, fn, program);
+        }
+
+        public void RegisterStudent(String ln, String fn, String mn, String gender, String day, String month, String year, String program)
+        {
+            MessageBox.Show("Student name: " + fn + " " + mn + " " + ln + "\nGender: " + gender + "\nDate of Birth " + day + " / " + month + " / " + year + "\nProgram " + program);
+        }
+
+        public void RegisterStudent(String ln, String fn, String mn, String program)
+        {
+            MessageBox.Show("Student Name: " + fn + " " + mn + " " + ln + "\nProgram: " + program);
+        }
+        public void RegisterStudent(String ln, String fn, String program)
+        {
+            MessageBox.Show("Student name: " + fn + " " + ln + "\nProgram " + program);
+        
+
+        }
+    }
+}
